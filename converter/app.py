de3aa11bda82b439c965b055e267034ffdcc373f
Felipe Leyva <d.leyvad@uniandes.edu.co>
2023-04-04 11:50:44 -0500

    Serialización

--- converter/app.py
+++ converter/app.py
@@ -1,5 +1,6 @@
 from converter import create_app
 from .model import db, User, Task
+from .model import UserSchema, TaskSchema
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,16 +10,14 @@ db.init_app(app)
 db.create_all()
 
 with app.app_context():
+    user_schema = UserSchema()
+    task_schema = TaskSchema()
     u = User(user_name='Felipe', email='d.leyvad@uniandes.edu.co', password='admin')
     t1 = Task(file_name='sample.zip', new_format='7z')
     t2 = Task(file_name='sample.7z', new_format='zip')
-
     u.tasks.append(t1)
     u.tasks.append(t2)
-
     db.session.add(u)
     db.session.commit()
-    print(User.query.all())
-    print(User.query.all()[0].tasks)
-    print(User.query.all())
-    print(Task.query.all())
+    print([user_schema.dumps(user) for user in User.query.all()])
+    print([task_schema.dumps(task) for task in Task.query.all()])
