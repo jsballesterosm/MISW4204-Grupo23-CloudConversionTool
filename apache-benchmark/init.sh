6d4034f7919f08d1a364874000a1f8f669972cae
Juan Sebastian Ballesteros Muñoz <j.ballesterosm@Juans-MacBook-Air.local>
2023-04-20 09:49:26 -0500

    ajustes

--- apache-benchmark/init.sh
+++ apache-benchmark/init.sh
@@ -3,7 +3,7 @@
 TOKEN=$1
 URL_TASK=$2
 
-
+TIME_SLEEP=1
 
 echo -e "---"
 echo -e "PRUEBAS DE CARGA"
@@ -11,13 +11,23 @@ echo "---"
 
 sh carga/n1000_c300.sh ${1} ${2}
 
+sleep $TIME_SLEEP
+
 sh carga/n2000_c300.sh ${1} ${2}
+
+sleep $TIME_SLEEP
 
 sh carga/n3000_c300.sh ${1} ${2}
 
+sleep $TIME_SLEEP
+
 sh carga/n4000_c300.sh ${1} ${2}
 
+sleep $TIME_SLEEP
+
 sh carga/n5000_c300.sh ${1} ${2}
+
+sleep $TIME_SLEEP
 
 echo -e "---"
 echo -e "PRUEBAS DE CONCURRENCIA"
@@ -25,10 +35,18 @@ echo "---"
 
 sh concurrencia/n500_c100.sh ${1} ${2}
 
+sleep $TIME_SLEEP
+
 sh concurrencia/n500_c200.sh ${1} ${2}
+
+sleep $TIME_SLEEP
 
 sh concurrencia/n500_c300.sh ${1} ${2}
 
+sleep $TIME_SLEEP
+
 sh concurrencia/n500_c400.sh ${1} ${2}
+
+sleep $TIME_SLEEP
 
 sh concurrencia/n500_c500.sh ${1} ${2}
